df341874b5a2d506173c14b1436f8797147b82b5
timonforrer <timon.forrer@gmail.com>
2021-07-22 07:32:22 +0200

    fetching pages in de+en, exposing as pages object

--- src/data/pages.js
+++ src/data/pages.js
@@ -1,22 +1,30 @@
+// plugin for caching network request (makes reloads during development more performant and reduces api requests)
 const Cache = require('@11ty/eleventy-cache-assets');
 
-module.exports = async function() {
-  // the preview site is a seperate netlify site with NODE_ENV=preview
-  const token = process.env.NODE_ENV == 'preview' ? process.env.storyblok_preview : process.env.storyblok_public;
-  const query = `starts_with=pages/`;
-  const url = `https://api.storyblok.com/v2/cdn/stories?${query}&token=${token}`;
+const baseURL = 'https://api.storyblok.com/v2/cdn/stories';
+// the preview site is a seperate netlify site with NODE_ENV=preview set
+const token = process.env.NODE_ENV == 'preview' ? process.env.storyblok_preview : process.env.storyblok_public;
 
-  const options = {
-    // if in preview environment, set cache duration to 5s to always get fresh data
-    duration: process.env.NODE_ENV == 'preview' ? '5s' : '1d',
-    type: 'json',
-    fetchOptions: {
-      headers: {
-        "Accept": "application/json",
-        "Content-Type": "application/json"
-      }
+const options = {
+  // if in preview environment, set cache duration to 5s to always get fresh data, else, cache for 1 day
+  duration: process.env.NODE_ENV == 'preview' ? '5s' : '1d',
+  type: 'json',
+  fetchOptions: {
+    headers: {
+      "Accept": "application/json",
+      "Content-Type": "application/json"
     }
   }
+}
 
+// function for retrieving content
+async function fetchPages(prefix) {
+  const url = `${baseURL}?starts_with=${prefix ?? ''}pages&token=${token}`;
   return Cache(url, options);
 }
+
+module.exports = async function() {
+  let de = await fetchPages();
+  let en = await fetchPages('en/');
+  return { de, en };
+};
