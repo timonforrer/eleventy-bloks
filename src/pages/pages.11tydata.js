c651d1be2707d0456c37d9895af1b2c62e8e8c7b
timonforrer <timon.forrer@gmail.com>
2021-07-22 15:39:46 +0200

    Added helper to expose webcomponents to frontmatter

--- src/pages/pages.11tydata.js
+++ src/pages/pages.11tydata.js
@@ -1,16 +1,17 @@
 // helper to resolve links
 const resolveLink = require('../helpers/linkResolver.js');
+const filterWebcomponents = require('../helpers/getWebcomponents.js');
 
 module.exports = {
   layout: "base.njk",
   eleventyComputed: {
-    permalink: (data) => {
+    permalink: data => {
       const {
         pages: all_pages,
         pdata: page_to_open
       } = data;
 
-      // appearantly, this also gets called when data not ready
+      // apparantly, this also gets called when data not ready
       if (Object.keys(all_pages).length > 0)  {
         return resolveLink(page_to_open, all_pages);
       } else {
@@ -18,5 +19,6 @@ module.exports = {
       }
     },
     title: data => data.pdata.content.meta_tags[0]?.title,
+    webcomponents: data => filterWebcomponents(data.pdata.content)
   }
 }
