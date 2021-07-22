b3db52f4d93cb5c27c4682c83039ca206c8945b4
timonforrer <timon.forrer@gmail.com>
2021-07-22 15:37:46 +0200

    Setting up webcomponents config (names + path)

--- .site.config.js
+++ .site.config.js
@@ -1,3 +1,9 @@
+const webcomponents_paths = {
+  // specify the path of the file, where each webcomponent gets defined (using `customElements.define`)
+  'sp-music-player': '@spartan-components/sp-music-player/sp-music-player.js',
+  'sp-video': '@spartan-components/sp-video-player/sp-video.js'
+}
+
 module.exports = {
   // eleventy directories
   dir: {
@@ -8,7 +14,8 @@ module.exports = {
     output: 'dist'
   },
 
-  // these use storyblok keys
+  // define language url prefixes
+  // use storybloks key for creating these objects
   languages: {
     default: {
       key: 'de',
@@ -18,5 +25,13 @@ module.exports = {
       key: 'en',
       prefix: '/en'
     }
+  },
+
+  // bloks in storyblok can require a webcomponent to be loaded
+  // for each blok, create a configuration object
+  // use the storyblock technical name as key, followed by the path of the webcomponent in question
+  webcomponents: {
+    youtube_video: webcomponents_paths['sp-video'],
+    tracks: webcomponents_paths['sp-music-player']
   }
 }
