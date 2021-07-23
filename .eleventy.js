9a1d963df983c3633a90e747f66c388524a7105a
timonforrer <timon.forrer@gmail.com>
2021-07-23 12:56:13 +0200

    Added config for Eleventy Serverless

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,11 @@
 const path = require('path');
 const alias = require('module-alias');
+const { EleventyServerlessBundlerPlugin } = require('@11ty/eleventy')
 
 // custom options can be required via @site_config
 // inspired by twelvety
 // https://github.com/gregives/Twelvety
-alias.addAlias('@site_config', path.join(__dirname, '.site.config'));
+alias.addAlias('@site_config', path.join(__dirname, '.site.config.js'));
 
 // requiring options from @siteConfig
 const site_config = require('@site_config');
@@ -17,6 +18,17 @@ module.exports = function(config) {
   // hooking up our filters to the config
   addFilters(config);
 
+  config.addPlugin(EleventyServerlessBundlerPlugin, {
+    name: 'dynamic',
+    inputDir: './src/',
+    functionsDir: './netlify/functions',
+    copy: [
+      { from: '.cache', to: 'cache' },
+      './.site.config.js',
+      './src/helpers/',
+      './src/pages/'
+    ]
+  })
   return {
     // if you want to use other templating languages other than liquid, add them to the array.
     templateFormats: ['njk'],
