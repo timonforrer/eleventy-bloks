a2840fbefdc7a54f8a2d7904428f08fc620ace3e
timonforrer <timon.forrer@gmail.com>
2021-07-26 13:48:23 +0200

    Should be fixed now?

--- src/pages/preview.11tydata.js
+++ src/pages/preview.11tydata.js
@@ -2,14 +2,7 @@
 module.exports = {
   eleventyComputed: {
     permalink: {
-      dynamic: data => {
-        // apparently, this also gets called when data not ready, so check if any pages available
-        if (Object.keys(data.pages).length > 0)  {
-          return ['/preview/*/','/preview/*']
-        } else {
-          return
-        }
-      }
+      dynamic: ['/preview/*','/preview/*/']
     }
   }
 }
