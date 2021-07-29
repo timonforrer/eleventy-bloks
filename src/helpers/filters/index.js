5aa976ef3e060130cc4395db7828d74f71b2820f
timonforrer <timon.forrer@gmail.com>
2021-07-29 13:44:54 +0200

    No longer needed, replaced by global routes object

--- src/helpers/filters/index.js
+++ src/helpers/filters/index.js
@@ -1,13 +1,11 @@
 // here you can require all your filters in order to expose them to the eleventy config
 const getYoutubeID = require('./getYoutubeID.js');
-const linkResolver = require('../linkResolver.js');
 const renderRichText = require('./renderRichText.js');
 const transformDate = require('./transformDate.js')
 
 module.exports = function(config) {
   // expose the filter to the eleventy config
   config.addFilter('getYoutubeID', getYoutubeID);
-  config.addFilter('resolveLink', linkResolver);
   config.addFilter('transformDate', transformDate);
   config.addFilter('renderRichText', renderRichText);
 }
--- src/pages/static.11tydata.js
+++ src/pages/static.11tydata.js
@@ -1,6 +1,3 @@
-// helper to resolve links
-// const resolveLink = require('../helpers/linkResolver.js');
-
 module.exports = {
   eleventyComputed: {
     permalink:  data => `${data.routes[data.pdata.lang_key][data.pdata.full_slug]}/index.html`
