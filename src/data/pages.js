99fe3154c5bed03d66724eec3b26635c5c85ac35
timonforrer <timon.forrer@gmail.com>
2021-07-29 11:42:09 +0200

    Replaced fetch logic with helper function

--- src/data/pages.js
+++ src/data/pages.js
@@ -1,34 +1,5 @@
-// plugin for caching network request (makes reloads during development more performant and reduces api requests)
-const Cache = require('@11ty/eleventy-cache-assets');
-const fetch = require('node-fetch');
 const site_config = require('@site_config');
-
-// function to fetch `stories` in a specific language from storyblok api
-// prefix is used to specify the language
-const fetchPage = async function(prefix) {
-  const baseURL = 'https://api.storyblok.com/v2/cdn/stories';
-
-  // if in serverless environment, use the preview token and set version=draft to get
-  // unpublished stories
-  const token =
-    process.env.ELEVENTY_SERVERLESS
-    ? `${process.env.storyblok_preview}&version=draft`
-    : process.env.storyblok_public;
-
-  const url = `${baseURL}?starts_with=${prefix}pages&token=${token}`;
-
-  // if in serverless environment, use fresh data, otherwise get cached results
-  if (process.env.ELEVENTY_SERVERLESS) {
-    const response = await fetch(url);
-    return response.json();
-  } else {
-    const response = await Cache(url, {
-      duration: '1d',
-      type: 'json'
-    });
-    return response;
-  }
-}
+const fetchFromStoryblok = require('../helpers/fetchFromStoryblok.js');
 
 // function to create object of pages with storyblok slug as key
 // --> slug as key is useful for creating dynamic previews
@@ -38,12 +9,12 @@ const buildObject = function(raw_pages) {
 
   for (let page of raw_pages) {
     // create detached copy of page (otherwise the lang attribute gets modified on the original which causes an error)
-    let custom_page = { ...page };
+    page = { ...page };
     // create lang_key (storyblok uses different values for `lang` instead of actual language)
     // in this case, `default` will be replaced with `de`
-    custom_page.lang_key = site_config.languages[page.lang].key;
+    page.lang_key = site_config.languages[page.lang].key;
     // add page to pages_object, set full_slug as key
-    pages_object[custom_page.full_slug] = custom_page;
+    pages_object[page.full_slug] = page;
   }
   return pages_object;
 }
@@ -59,7 +30,7 @@ module.exports = async function() {
     // get relevant config
     let { fetchPrefix } = languages[lang];
     // fetch with fetchPrefix set
-    const pages_by_lang = await fetchPage(fetchPrefix);
+    const pages_by_lang = await fetchFromStoryblok(`${fetchPrefix}pages`);
     // add to array
     raw_pages.push(...pages_by_lang.stories);
   }
