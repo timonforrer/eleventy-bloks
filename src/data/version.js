87b7150e139ad0a193c028fcd3f4831e7b94a39e
timonforrer <timon.forrer@gmail.com>
2021-08-01 14:55:11 +0200

    Maybe scripts have to run sequential

--- src/data/version.js
+++ src/data/version.js
@@ -1,8 +1,9 @@
 const { readdirSync } = require('fs');
-const dir = process.env.ELEVENTY_SERVERLESS
-  ? readdirSync('.netlify/functions/dynamic/src/assets/bundled')
-  : readdirSync('./src/assets/bundled');
+// const dir = process.env.ELEVENTY_SERVERLESS
+//   ? readdirSync('.netlify/functions/dynamic/src/assets/bundled')
+//   : readdirSync('./src/assets/bundled');
 console.log(readdirSync('.'));
 
+const dir = readdirSync('./src/assets/bundled');
 
 module.exports = dir[0].replace(/\./g, '-');
