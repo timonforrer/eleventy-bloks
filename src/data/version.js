ca51e5616a96f2cf7313ed622516d17b27da9bd3
timonforrer <timon.forrer@gmail.com>
2021-08-01 14:19:00 +0200

    Trying to access asset folder inside function

--- src/data/version.js
+++ src/data/version.js
@@ -1,5 +1,8 @@
 const { readdirSync } = require('fs');
-const dir = readdirSync('./src/assets/bundled');
+const dir = process.env.ELEVENTY_SERVERLESS
+  ? readdirSync('.netlify/functions/dynamic/src/assets/bundled')
+  : readdirSync('./src/assets/bundled');
 console.log(readdirSync('.'));
+
 
 module.exports = dir[0].replace(/\./g, '-');
