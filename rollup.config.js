a3b62e60d9bce65e604fe4d0a5859bb9611fe319
timonforrer <timon.forrer@gmail.com>
2021-08-19 19:08:46 +0200

    Now exposing version from site_config

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,24 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
+import minifyHTML from 'rollup-plugin-minify-html-literals'
+import { terser } from 'rollup-plugin-terser';
 
-// get version from package.json – used for cache busting
-let { version } = require('./package.json');
-version = version.replace(/\./g, '-');
 
-let { webcomponents } = require('./site.config.js');
+// get webcomponents and version
+let { webcomponents, version } = require('./site.config.js');
+// turn webcomponents object into array of paths
 webcomponents = Object.values(webcomponents);
 
 export default {
+  // input array of webcomponents, to make use of automatic tree shaking
   input: webcomponents,
   output: {
+    // specify output folder using site version
     dir: `src/assets/bundled/${version}/js`,
     format: 'es'
   },
-  plugins: [nodeResolve()]
+  plugins: [
+    nodeResolve(),
+    minifyHTML(),
+    terser()
+  ]
 }
--- site.config.js
+++ site.config.js
@@ -1,5 +1,11 @@
+// get version from package.json
+let { version } = require('./package.json');
+// replace dots with dashes, so it can be used for folder names
+version = version.replace(/\./g, '-');
+
+// specify the path of the files, where each webcomponent gets defined (using `customElements.define`)
+// use the tag name of the component as the key
 const webcomponents_paths = {
-  // specify the path of the file, where each webcomponent gets defined (using `customElements.define`)
   'sp-music-player': '@spartan-components/sp-music-player/sp-music-player.js',
   'sp-video': '@spartan-components/sp-video/sp-video.js'
 }
@@ -13,9 +19,8 @@ module.exports = {
     layouts: 'layouts',
     output: 'dist'
   },
-
   // define language url prefixes
-  // use storybloks key for creating these objects
+  // use storybloks key as object keys
   languages: {
     default: {
       key: 'de',
@@ -28,12 +33,15 @@ module.exports = {
       fetchPrefix: 'en/'
     }
   },
-
   // bloks in storyblok can require a webcomponent to be loaded
   // for each blok, create a configuration object
   // use the storyblock technical name as key, followed by the path of the webcomponent in question
   webcomponents: {
     youtube_video: webcomponents_paths['sp-video'],
-    tracks: webcomponents_paths['sp-music-player']
-  }
+    tracks: webcomponents_paths['sp-music-player'],
+    storage: webcomponents_paths['app-localstorage-document']
+  },
+  
+  // return the package version for cache busting of assets
+  version: version
 }
