0db5bf5ad0d7a076d49116e15a567238bbee39a8
timonforrer <timon.forrer@gmail.com>
2021-08-19 19:07:48 +0200

    Added sass/css processing

---
+++ src/helpers/styles.js
@@ -0,0 +1,42 @@
+const sass = require('sass');
+const postCss = require('postcss');
+
+// postcss plugins
+const postcssPresetEnv = require('postcss-preset-env');
+const postCssImport = require('postcss-import');
+
+const fs = require('fs-extra');
+
+// get version of site for cache busting
+const { version } = require('@site_config');
+
+module.exports = function(config) {
+
+  const processCss = () => {
+
+    let result = sass.renderSync({
+      file: 'src/styles/main.scss',
+      sourceMap: false,
+      outputStyle: 'compressed'
+    });
+
+    let css = result.css.toString();
+
+    postCss([
+      postcssPresetEnv,
+      postCssImport
+    ])
+      .process(css, { from: 'src/styles/main.scss', to: `src/assets/bundled/${version}/css/main.css` })
+      .then(result => {
+        fs.outputFile(`src/assets/bundled/${version}/css/main.css`, result.css, err => {
+          if (err) throw err;
+          console.log("CSS optimized");
+        });
+      });
+  }
+
+  // run before first build
+  config.on('beforeBuild', () => processCss());
+  // and before every watch
+  config.on('beforeWatch', () => processCss());
+}
