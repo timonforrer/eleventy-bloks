a3f879b922cbe19b510712b29b25a47c56d6eb40
timonforrer <timon.forrer@gmail.com>
2021-08-27 07:42:07 +0200

    Added filter to remove double slashes from urls

--- src/helpers/filters/index.js
+++ src/helpers/filters/index.js
@@ -1,11 +1,13 @@
 // here you can require all your filters in order to expose them to the eleventy config
 const getYoutubeID = require('./getYoutubeID.js');
+const removeDoubleSlashes = require('./removeDoubleSlashes.js');
 const renderRichText = require('./renderRichText.js');
 const transformDate = require('./transformDate.js')
 
 module.exports = function(config) {
   // expose the filter to the eleventy config
   config.addFilter('getYoutubeID', getYoutubeID);
+  config.addFilter('removeDoubleSlashes', removeDoubleSlashes);
   config.addFilter('transformDate', transformDate);
   config.addFilter('renderRichText', renderRichText);
 }
