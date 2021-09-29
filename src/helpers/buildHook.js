67f008c1f1c61adffe32df80c3d4fa161d4302bb
timonforrer <timon.forrer@gmail.com>
2021-09-29 13:31:19 +0200

    Only require process css logic if not in preview

--- src/helpers/buildHook.js
+++ src/helpers/buildHook.js
@@ -1,8 +1,7 @@
-const processCss = require('../styles');
-
 module.exports = function(config) {
   // only process styles if not inside serverless function
   if (!process.env.ELEVENTY_SERVERLESS) {
+    const processCss = require('../styles/index.js');
     // run before build
     config.on('beforeBuild', () => { processCss() });
   }
