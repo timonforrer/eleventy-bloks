47ed56e669d73be41bb20233f33dd54a04ccab8b
timonforrer <timon.forrer@gmail.com>
2021-10-08 00:28:48 +0200

    Move and rename responsiveImage to genericImage

--- src/helpers/shortcodes/images/genericImage.js
+++ src/helpers/shortcodes/images/genericImage.js
@@ -1,4 +1,9 @@
-const renderAttributes = require('../renderAttributes.js');
+const renderAttributes = require('../../renderAttributes.js');
+
+// sizes tell the browser which images to load
+// `1/1` is a full width image, `1/4` is one fourth of the page width
+// the `(min-width)` media queries might need to be adjusted to fit layout breakpoints
+const sizes = require('./sizes.json');
 
 module.exports = async function(params) {
 
@@ -12,17 +17,6 @@ module.exports = async function(params) {
     preview_src
   } = params;
 
-  // these sizes tell the browser which images to load
-  // `1/1` is a full width image, `1/4` is one fourth of the page width
-  // the `(min-width)` media queries might need to be adjusted to fit layout breakpoints
-  const sizes = {
-    'full_bleed': '100vw',
-    '1/1': '(min-width: 1000px) 95ch, 100vw',
-    '1/2': '(min-width: 920px) 50vw, 100vw',
-    '1/3': '(min-width: 920px) calc(100vw / 3), (min-width: 600px) 50vw, 100vw',
-    '1/4': '(min-width: 920px) 25vw, (min-width: 650px) calc(100vw / 3), (min-width: 440px) 50vw, 100vw'
-  }
-
   // alt has to be specified, for decorative images use `alt=""`
   if (alt === undefined) throw new Error(`Alt tag missing on img with src: ${src}`);
 
@@ -35,18 +29,22 @@ module.exports = async function(params) {
 
   // check if in serverless environment
   if(process.env.ELEVENTY_SERVERLESS) {
+
     // if true, return only minimally optimised image
     return `
       <picture ${renderAttributes(picture_attributes)}>
         <img ${renderAttributes(img_attributes)} src="${preview_src}" />
       </picture>`;
+
   } else {
+
     // else, we can require the Image function
     // (if loaded in serverless env, we'd be exceeding the memory limit)
     const Image = require("@11ty/eleventy-img");
 
     // generate images and get metadata about it (paths, dimensions etc.)
     let generated_images = await Image(src, options);
+
     // construct the html
     return `
       <picture ${renderAttributes(picture_attributes)}>
@@ -66,6 +64,7 @@ module.exports = async function(params) {
           height="${generated_images['jpeg'][0].height}"
           loading="lazy"
           decoding="async"
+          alt="${alt}"
           ${renderAttributes(img_attributes)}
         />
       </picture>`;
