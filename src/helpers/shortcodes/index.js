e2df42d9189528484a488322e80d6053e0c70aa0
timonforrer <timon.forrer@gmail.com>
2021-10-31 17:17:13 +0100

    Add preview shortcode

--- src/helpers/shortcodes/index.js
+++ src/helpers/shortcodes/index.js
@@ -1,5 +1,9 @@
+const { preview, endpreview } = require('./preview.js');
+
 module.exports = function(config) {
   // add shortcodes to eleventy config
   config.addNunjucksAsyncShortcode('image', require('./images/genericImage.js'));
   config.addNunjucksAsyncShortcode('storyblok_image', require('./images/dynamicStoryblokImage.js'));
+  config.addShortcode('preview', preview);
+  config.addShortcode('endpreview', endpreview);
 }
