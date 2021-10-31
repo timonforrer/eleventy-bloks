4f009e1e84406932a8f214ff4386f78a96908378
timonforrer <timon.forrer@gmail.com>
2021-10-31 10:08:38 +0100

    Make 'art direction' working

--- src/helpers/shortcodes/images/dynamicStoryblokImage.js
+++ src/helpers/shortcodes/images/dynamicStoryblokImage.js
@@ -3,115 +3,101 @@ const renderAttributes = require('../../renderAttributes.js');
 // sizes tell the browser which images to load
 // `1/1` is a full width image, `1/4` is one fourth of the page width
 // the `(min-width)` media queries might need to be adjusted to fit layout breakpoints
-const sizes = require('./sizes.json');
+const { sizes, media } = require('./config.js');
+// --> probably breaks the media queries for art direction..
 
+// get utils to do the transform magic
 const { transform } = require('@sporkbytes/storyblok-image');
-
-const get_height = (aspect, width) => {
-  const sides = aspect.split(':');
-  const width_base = Number(sides[0]);
-  const height_base = Number(sides[1]);
-  const aspect_decimal = height_base / width_base;
-  return { height: Math.round(width * aspect_decimal) };
-}
-
-const get_focal_points = (storyblok_focus, src) => {
-
-  // turn this: 'https://a.storyblok.com/f/119172/3000x1687/6535529a52/xyz.jpg'
-  // into this: [ 3000, 1687 ]
-  let original_image_size = src.split('/')[5].split('x');
-
-  original_image_size = {
-    width: Number(original_image_size[0]),
-    height: Number(original_image_size[1])
-  };
-
-  // turn this: '1542x787:1543x788'
-  // into this: [ 1542, 787 ]
-  let focus_coordinates = storyblok_focus.split(':')[0].split('x');
-
-  focus_coordinates = {
-    x: Number(focus_coordinates[0]),
-    y: Number(focus_coordinates[1])
-  };
-
-  return {
-    // return percentual coordinates
-    focalPointX: Math.round((focus_coordinates.x / original_image_size.width) * 100),
-    focalPointY: Math.round((focus_coordinates.y / original_image_size.height) * 100),
-  }
-}
+const { get_focal_points, get_height } = require('../../storyblokImageTransforms.js');
 
 module.exports = async function(params) {
 
+  // get params
   const {
-    src,
     alt,
-    storyblok_focus,
-    picture_attributes,
     img_attributes,
+    loading = 'lazy',
+    picture_attributes,
+    // set default for size
     size = '1/1',
-    aspect_phone,
-    aspect_tablet,
-    aspect_desktop
+    // access nested properties of `storyblok_data` and assign to new variables if needed
+    storyblok_data: {
+      aspect_mobile,
+      aspect_tablet,
+      aspect_desktop,
+      focus: storyblok_focus,
+      image: {
+        filename: src
+      }
+    },
   } = params;
 
   // alt has to be specified, for decorative images use `alt=""`
   if (alt === undefined) throw new Error(`Alt tag missing on img with src: ${src}`);
 
   let options = {
+
+    // if a focus is set in storyblok, transform the values to usable format using `get_focal_points`
     ...(storyblok_focus && {
       focal_points: get_focal_points(storyblok_focus, src)
     }),
+
+    // options for eleventy img
     eleventy: {
       outputDir: 'dist/img/',
       formats: ['avif', 'webp', 'jpg'],
     },
+
+    // specify the desired outputs
+    // nullish values will be populated later
     output: [
       {
-        aspect: aspect_phone,
+        aspect: aspect_mobile,
         sizes: {
           width: 800,
-        }
+          height: null,
+        },
+        url: null
       },
       {
         aspect: aspect_tablet,
         sizes: {
           width: 1200,
-        }
+          height: null,
+        },
+        url: null
       },
       {
         aspect: aspect_desktop,
         sizes: {
           width: 1600,
-        }
+          height: null,
+        },
+        url: null
       },
       {
         aspect: aspect_desktop,
         sizes: {
           width: 2000,
-        }
+          height: null,
+        },
+        url: null
       }
     ]
   };
 
-  // quite ugly…?
-  options.output = options.output.map(item => {
-    const return_obj = {
-      aspect: item.aspect,
-      sizes: {
-        width: item.sizes.width,
-        ...(item.aspect && get_height(item.aspect, item.sizes.width))
-      }
-    }
-    const url = transform(src, { ...options.focal_points, ...return_obj.sizes });
-    return {
-      url: url,
-      ...return_obj,
-    };
-  });
+  // populate nullish values of output config
+  options.output.forEach((item, index) => {
 
-  console.log(storyblok_focus, options.focal_points);
+    // get reference to the actual object we wanna change
+    const mod_item = options.output[index];
+
+    // check if aspect specified, if yes: calculate height, else: return null
+    mod_item.sizes.height = item.aspect ? get_height(item.aspect, item.sizes.width) : null;
+
+    // generate url for each output option
+    mod_item.url = transform(src, { ...options.focal_points, ...item.sizes });
+  });
 
   // check if in serverless environment
   if(process.env.ELEVENTY_SERVERLESS) {
@@ -123,12 +109,21 @@ module.exports = async function(params) {
     // if true, return only minimally optimised image
     return `
       <picture ${renderAttributes(picture_attributes)}>
-        <source
-          type="image/jpeg"
-          srcset="${srcset}"
-          sizes="${sizes[size]}"
-        />
-        <img ${renderAttributes(img_attributes)} src="${options.output[0].url}" />
+        ${options.output.map(format => {
+          return`
+            <source
+              type="image/jpg"
+              ${
+                // if aspect specified for any device type, switch to media queries
+                // else, use sizes attribute
+                options.output.some(item => item.aspect)
+                ? `media="${media[format.sizes.width]}" srcset="${format.url}"`
+                : `sizes="${sizes[size]}" srcset="${format.url} ${format.sizes.width}w"`
+              }
+            />
+          `
+        }).join('\n')}
+        <img ${renderAttributes(img_attributes)} src="${options.output[0].url}" loading="${loading}" />
       </picture>`;
 
   } else {
@@ -139,38 +134,47 @@ module.exports = async function(params) {
 
     // generate images and get metadata about it (paths, dimensions etc.)
     let arr_generated_images = await Promise.all(options.output.map(async item => {
-      let image = await Image(item.url, options.eleventy)
+      let image = await Image(item.url, options.eleventy);
       return image;
     }));
 
+    // placeholder object, will be populated from the arr_generated_images
     let obj_generated_images = {};
 
+    // populate object, so it matches eleventy generated object
     arr_generated_images.forEach(set => {
       for(let key in set) {
         const data = set[key][0];
         if(obj_generated_images[key] === undefined) obj_generated_images[key] = [];
         obj_generated_images[key].push(data);
       }
-    })
+    });
 
     // construct the html
     return `
       <picture ${renderAttributes(picture_attributes)}>
         ${Object.values(obj_generated_images).map(format => {
-          return`
-            <source
-              type="${format[0].sourceType}"
-              srcset="${format.map(sources => sources.srcset).join(', ')}"
-              sizes="${sizes[size]}"
-            />
-          `;
+          return format.map(source => {
+            return`
+              <source
+                type="${source.sourceType}"
+                ${
+                  // if aspect specified for any device type, switch to media queries
+                  // else, use sizes attribute
+                  options.output.some(item => item.aspect)
+                  ? `media="${media[source.width]}" srcset="${source.url}"`
+                  : `sizes="${sizes[size]}" srcset="${source.srcset}"`
+                }
+              />
+            `;
+          }).join('\n');
         }).join('\n')}
 
         <img
           src="${obj_generated_images['jpeg'][0].url}"
           width="${obj_generated_images['jpeg'][0].width}"
           height="${obj_generated_images['jpeg'][0].height}"
-          loading="lazy"
+          loading="${loading}"
           decoding="async"
           alt="${alt}"
           ${renderAttributes(img_attributes)}
