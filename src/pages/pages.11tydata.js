4983745371d44cbc31fdc3d46dfc1ad911bd4b15
timonforrer <timon.forrer@gmail.com>
2021-11-02 21:21:06 +0100

    Re-introduce code that I thought was unnecessary

--- src/pages/pages.11tydata.js
+++ src/pages/pages.11tydata.js
@@ -1,7 +1,11 @@
+// utility to check if any webcomponents on page
+const filterWebcomponents = require('../helpers/getWebcomponents.js');
+
 module.exports = {
   layout: 'base.njk',
   eleventyComputed: {
     title: data => data.pdata.content.meta_tags[0]?.title,
     lang: data => data.pdata.lang_key,
+    webcomponents: data => filterWebcomponents(data.pdata.content)
   }
 }
