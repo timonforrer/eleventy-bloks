5da39e7cf040dc3cdc7542515e71a50ff4cd88b0
timonforrer <timon.forrer@gmail.com>
2021-11-02 21:22:39 +0100

    Fix exception that rendered `undefined` in front-end

--- src/helpers/shortcodes/preview.js
+++ src/helpers/shortcodes/preview.js
@@ -5,12 +5,16 @@ const preview = (data) => {
       <div class="preview-wrapper">
       ${blok._editable}
     `;
+  } else {
+    return '';
   }
 }
 
 const endpreview = () => {
   if(process.env.ELEVENTY_SERVERLESS) {
     return `</div>`;
+  } else {
+    return '';
   }
 }
 
