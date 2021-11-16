479c06f8fec63c8645c06bd8dc1ad3d23401b065
timonforrer <timon.forrer@gmail.com>
2021-11-16 07:38:58 +0100

    Fix getWebcomponent helper

--- src/helpers/getWebcomponents.js
+++ src/helpers/getWebcomponents.js
@@ -9,17 +9,18 @@ module.exports = function(page_content) {
   const toplevel_bloks = Object.keys(page_content);
 
   // get all nested blok names (inside content.body of a story)
-  const nested_bloks = page_content.body.map(item => item.component);
+  const nested_bloks = page_content.body?.map(item => item.component);
 
   // merge blok names
-  const bloks = [page_type, ...toplevel_bloks, ...nested_bloks];
+  const bloks = [page_type, ...toplevel_bloks, ...(nested_bloks !== undefined ? nested_bloks : [])];
 
   // get list of valid webcomponents
   const webcomponents = site_config.webcomponents.map(webcomponent => webcomponent.blok);
 
   // filter out valid blok names based on webcomponent list
   const filtered = bloks.filter(item => webcomponents.includes(item));
+  const deduped = [... new Set(filtered)];
 
   // return sources
-  return filtered.map(item => site_config.webcomponents[item]);
+  return deduped;
 }
